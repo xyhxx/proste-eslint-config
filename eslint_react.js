ccff36f2da6a5ddddaab197a0dbd66a46684b597
肖 <603331487@qq.com>
2021-08-22 20:44:58 +0800

    添加hooks规则

--- eslint_react.js
+++ eslint_react.js
@@ -9,7 +9,8 @@ module.exports = {
   },
   "extends": [
     "eslint:recommended",
-    "plugin:react/recommended"
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended"
   ],
   "parserOptions": {
     "sourceType": "module",
--- eslint_react_ts.js
+++ eslint_react_ts.js
@@ -11,6 +11,7 @@ module.exports = {
   "extends": [
     "eslint:recommended",
     "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
     "plugin:@typescript-eslint/eslint-recommended",
     "plugin:@typescript-eslint/recommended"
   ],
