21ddbabb45bd91a61b6db7dfcb5291f2648bb8ae
肖 <603331487@qq.com>
2022-04-09 16:29:53 +0800

    set @typescript-eslint/no-unsafe-return to 0

--- base-ts.js
+++ base-ts.js
@@ -11,7 +11,7 @@ module.exports = {
     "@typescript-eslint/no-implied-eval": 2, // 禁止eval
     "@typescript-eslint/no-extra-semi": 2, // 禁止多余冒号
     "@typescript-eslint/no-empty-function": 2, // 禁止空函数
-    "@typescript-eslint/no-unsafe-return": 1, // 不允许返回any类型
+    "@typescript-eslint/no-unsafe-return": 0, // 不允许返回any类型
     "@typescript-eslint/no-unnecessary-type-assertion": 1, // 类型断言没有改变类型时警告
     "@typescript-eslint/no-this-alias": [
       "error",
