b131154c4761b601731a3e0a48a8d88d8d8e0605
xyh <603331487@qq.com>
2024-08-03 14:56:22 +0800

    fix: rm commot

--- src/configs/unicorn.ts
+++ src/configs/unicorn.ts
@@ -3,8 +3,6 @@ import rules from '@rules/unicorn';
 import type {BaseConfigOptions} from '@utils/types';
 
 export async function getUnicornConfig({overrides}: BaseConfigOptions) {
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-expect-error
   const {default: unicorn} = await import('eslint-plugin-unicorn');
 
   const config: Linter.FlatConfig = {
